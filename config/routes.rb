b5a26a20d20c33c352d560ffa302d03459893870
duypt <duypt@fsoft.com.vn>
2013-02-26 13:08:27 +0700

    3th commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 # get 'polls', :to => 'polls#index'
-get 'polls', :to => 'polls#index'
+#get 'polls', :to => 'polls#index'
+#post 'post/:id/vote', :to => 'polls#vote'
+if Rails::VERSION::MAJOR < 3
+  ActionController::Routing::Routes.draw do |map|
+    #map.connect 'adburndown/:action/:id', :controller => :adburndown
+  map.connect 'polls/:action/:id', :controller => :polls
+  end
+else
+  #match 'adburndown/(:action(/:id))', :controller => 'adburndown'
+  get 'polls', :to => 'polls#index'
 post 'post/:id/vote', :to => 'polls#vote'
+end
